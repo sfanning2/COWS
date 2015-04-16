f67edb08dc2d732ac081d591313bdbf2cc3efc07
lbush <lynneabush@gmail.com>
2015-04-15 21:32:04 -0500

    Update sliders for more reasonable values

--- Cows/src/cows/relogo/UserGlobalsAndPanelFactory.groovy
+++ Cows/src/cows/relogo/UserGlobalsAndPanelFactory.groovy
@@ -12,9 +12,9 @@ public class UserGlobalsAndPanelFactory extends AbstractReLogoGlobalsAndPanelFac
 	 */
 	public void addGlobalsAndPanelComponents(){
 
-		addSliderWL("numHerders", "Number of Herders", 1, 1, 100, 1)
-		addSliderWL("numCows", "Number of Cows", 5, 5, 250, 5)
-		addSliderWL("obstacleDensity", "Density of Obstacles", 0, 0.01, 0.5, 0)
-		addMonitorWL("remainingCows", "Cows in Field", 5)	
+		addSliderWL("numHerders", "Number of Herders", 1, 1, 50, 1)
+		addSliderWL("numCows", "Number of Cows", 1, 5, 100, 5)
+		addSliderWL("obstacleDensity", "Density of Obstacles", 0, 0.01, 0.25, 0)
+		addMonitorWL("remainingCows", "Cows in Field", 1)	
 	}
 }
