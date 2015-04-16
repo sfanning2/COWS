e30b8d1cf971221b9f274bb5130bf762bb924cd1
lbush <lynneabush@gmail.com>
2015-04-15 21:15:56 -0500

    More comments

--- Cows/src/cows/factories/ReLogoPanelCreator.java
+++ Cows/src/cows/factories/ReLogoPanelCreator.java
@@ -3,9 +3,15 @@ package cows.factories;
 import javax.swing.JPanel;
 import repast.simphony.relogo.factories.AbstractReLogoPanelCreator;
 import cows.relogo.UserGlobalsAndPanelFactory;
-
+/**
+ * Object for creating panels
+ * @author Lynnea
+ *
+ */
 public class ReLogoPanelCreator extends AbstractReLogoPanelCreator {
-
+	/**
+	 * Add globals and controls to panel
+	 */
 	public void addComponents(JPanel parent) {
 		UserGlobalsAndPanelFactory ugpf = new UserGlobalsAndPanelFactory();
 		ugpf.initialize(parent);
